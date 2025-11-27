9326ba8764c62b55e54b93780b8308e5bcaa2771
x3nonite <238069439+x3nonite@users.noreply.github.com>
2025-11-27 11:23:06 +0100

    improve swift support

--- Package.swift
+++ Package.swift
@@ -4,45 +4,40 @@ import PackageDescription
 let package = Package(
     name: "TreeSitterLatex",
     platforms: [.macOS(.v10_13), .iOS(.v11)],
-    products: [
-        .library(name: "TreeSitterLatex", targets: ["TreeSitterLatex"]),
-    ],
-    dependencies: [],
+    products: [.library(name: "TreeSitterLatex", targets: ["TreeSitterLatex"])],
     targets: [
-        .target(name: "TreeSitterLatex",
-                path: ".",
-                exclude: [
-                    "Cargo.toml",
-                    "Makefile",
-                    "binding.gyp",
-                    "bindings/c",
-                    "bindings/go",
-                    "bindings/node",
-                    "bindings/python",
-                    "bindings/rust",
-                    "prebuilds",
-                    "grammar.js",
-                    "package.json",
-                    "package-lock.json",
-                    "pyproject.toml",
-                    "setup.py",
-                    "test",
-                    "examples",
-                    ".editorconfig",
-                    ".github",
-                    ".gitignore",
-                    ".gitattributes",
-                    ".gitmodules",
-                ],
-                sources: [
-                    "src/parser.c",
-                    "src/scanner.c",
-                ],
-                resources: [
-                    .copy("queries")
-                ],
-                publicHeadersPath: "bindings/swift",
-                cSettings: [.headerSearchPath("src")])
-    ],
-    cLanguageStandard: .c11
+        .target(
+            name: "TreeSitterLatex",
+            path: ".",
+            exclude: [
+                "Cargo.toml",
+                "Makefile",
+                "binding.gyp",
+                "bindings/c",
+                "bindings/go",
+                "bindings/node",
+                "bindings/python",
+                "bindings/rust",
+                "grammar.js",
+                "package.json",
+                "package-lock.json",
+                "pyproject.toml",
+                "setup.py",
+                "test",
+                "examples",
+                ".editorconfig",
+                ".github",
+                ".gitignore",
+                ".gitattributes"
+            ],
+            sources: [
+                "src/parser.c",
+                "src/scanner.c",
+            ],
+            resources: [
+//                .copy("queries")
+            ],
+            publicHeadersPath: "bindings/swift",
+            cSettings: [.headerSearchPath("src")])
+    ]
 )
--- bindings/swift/TreeSitterLatex/latex.h
+++ bindings/swift/TreeSitterLatex/latex.h
@@ -7,7 +7,7 @@ typedef struct TSLanguage TSLanguage;
 extern "C" {
 #endif
 
-const TSLanguage *tree_sitter_latex(void);
+extern TSLanguage *tree_sitter_latex();
 
 #ifdef __cplusplus
 }
